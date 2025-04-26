4905e5882894af7c6710e59d2ab3c68927559cdb
ahmedEssyad <abdoulsidi876@gmail.com>
2025-04-26 06:18:50 +0000

    okko

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // MongoDB Connection
-const MONGODB_URI = 'mongodb+srv://abdoulsidi876:JeSuisMedy6002@cluster0.sf9qf8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://abdoulsidi876:JeSuisMedy6002@cluster0.sf9qf8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/voting-app')
+mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
